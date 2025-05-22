bea3d127febcc58ac926a4c7ed2605cb7b962586
diandrade <contato.andradediego@gmail.com>
2025-05-22 10:54:54 -0300

    Adição de contato.js dentro da contato.html.

---
+++ js/contato.js
@@ -0,0 +1,7 @@
+const telContainer = document.querySelector('.contact-tel');
+const emailContainer = document.querySelector('.contact-email');
+const enderecoContainer = document.querySelector('.contact-endereco');
+
+telContainer.addEventListener('click', () => {
+    console.log("Clicked!");
+})
